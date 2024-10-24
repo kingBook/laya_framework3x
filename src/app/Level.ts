7b6a5858038acc27ac8227a84eb9a9d3984f4bf4
kingBook <412126604@qq.com>
2024-10-25 03:48:13 +0800

    update 2024-10-25 03:48

--- src/app/Level.ts
+++ src/app/Level.ts
@@ -7,13 +7,20 @@ const { regClass, property } = Laya;
 @regClass()
 export class Level extends Laya.Script {
 
+    private static s_instance: Level;
     declare owner: Laya.Sprite;
     private _fsm: LevelFsm;
 
+    public static get instance(): Level { return Level.s_instance }
     public get fsm(): LevelFsm { return this._fsm; }
 
     onAwake(): void {
+        Level.s_instance = this;
         this._fsm = NodeUtil.addNodeComponent(LevelFsm, this.owner);
+    }
+
+    onDestroy(): void {
+        Level.s_instance = null;
     }
 
 }
